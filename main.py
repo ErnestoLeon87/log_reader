1b3dd642ab02f2e7ad6f739e7d3a709a2551f7f4
Ernesto Leon <45864173+ErnestoLeon87@users.noreply.github.com>
2019-12-04 11:27:28 -0500

    readLog function

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
 """
 Main description
 """
-import re
-regex = r''
-
-def log_reader(log_path):
-    pass
--- reader.py
+++ reader.py
@@ -6,14 +6,11 @@ path = "smsts.log"
 class Log_Reader:
     def __init__(self, logPath: "Log file path"):
         self.logPath = logPath
-        self.list = []
+        self.loglist = []
         self.failedRegex = r""
         self.nameRegex = r"\[+[\w\s\d\t\$.:\\'-_\]]+"
-
         self.timeRegex = r"[0-9]+:[0-9]+:[0-9]+\.[0-9]+\+[0-9]+"
-
         self.dateRegex = r"[0-9]{2}-[0-9]{2}-[0-9]{4}"
-
         self.errorCodeRegex = r"0x[A-F0-9]{8}"
 
     def readLogName(self):
@@ -22,7 +19,7 @@ class Log_Reader:
                 for item in re.finditer(self.nameRegex, logItem, re.S):
                     math_text = item.group()
                     math_text = math_text[5: (len(math_text)-5)]
-                    self.list.append(math_text)
+                    self.loglist.append(math_text)
 
     def readErrorCode(self):
         with open(self.logPath, "r") as log:
@@ -30,10 +27,18 @@ class Log_Reader:
                 for item in re.finditer(self.errorCodeRegex, logItem, re.S):
                     math_text = item.group()
                     math_text = math_text[0:]
-                    self.list.append(math_text)
+                    self.loglist.append(math_text)
+
+    def readLog(self):
+        with open(self.logPath) as log:
+            for item in log:
+                if item.__contains__("failed"):
+                    self.loglist.append(item)
 
 
 logreader = Log_Reader(path)
 # logreader.readLogName()
-logreader.readErrorCode()
-print(logreader.list)
+# logreader.readErrorCode()
+logreader.readLog()
+# print(logreader.list)
+print(logreader.loglist[0:2])
