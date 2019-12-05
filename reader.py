832cceddecced5246fcb9e41d18440f34cbd2e7d
Ernesto Leon <45864173+ErnestoLeon87@users.noreply.github.com>
2019-12-05 09:16:32 -0500

    12-5-19

--- reader.py
+++ reader.py
@@ -3,22 +3,55 @@ import re
 path = "smsts.log"
 
 
-class Log_Reader:
-    def __init__(self, logPath: "Log file path"):
+class LogReader:
+    def __init__(self, logPath: "", readlog=None):
         self.logPath = logPath
         self.loglist = []
+        self.threadRegex = r"thread=\"[0-9]+\""
         self.failedRegex = r""
         self.nameRegex = r"\[+[\w\s\d\t\$.:\\'-_\]]+"
         self.timeRegex = r"[0-9]+:[0-9]+:[0-9]+\.[0-9]+\+[0-9]+"
         self.dateRegex = r"[0-9]{2}-[0-9]{2}-[0-9]{4}"
         self.errorCodeRegex = r"0x[A-F0-9]{8}"
+        if readlog == None:
+            self.loglist = self.__readlog()
+
+    def __readlog(self):
+        listResult = []
+        with open(self.logPath, "r") as log:
+            for item in log:
+                if ("Failed" in item) or ("failed" in item):
+                    listResult.append(item)
+        return listResult
+
+    def getThreadFailed(self):
+        listResult = []
+        with open(self.logPath, "r") as log:
+            patternThread = re.compile(self.threadRegex)
+            for item in log:
+                if ("Failed" in item) or ("failed" in item):
+                    found = False
+                    itemFound = patternThread.findall(item)
+                    if len(listResult) == 0:
+                        listResult.append(itemFound)
+                    else:
+                        for r in listResult:
+                            if itemFound[0] in r:
+                                found = True
+                            else:
+                                found = False
+                        if not found:
+                            listResult.append(itemFound)
+                else:
+                    pass
+        return listResult
 
     def readLogName(self):
         with open(self.logPath, "r") as log:
             for logItem in log:
                 for item in re.finditer(self.nameRegex, logItem, re.S):
                     math_text = item.group()
-                    math_text = math_text[5: (len(math_text)-5)]
+                    math_text = math_text[5: (len(math_text) - 5)]
                     self.loglist.append(math_text)
 
     def readErrorCode(self):
@@ -28,12 +61,6 @@ class Log_Reader:
                     math_text = item.group()
                     math_text = math_text[0:]
                     self.loglist.append(math_text)
-
-    def readLog(self):
-        with open(self.logPath) as log:
-            for item in log:
-                if item.__contains__("failed"):
-                    self.loglist.append(item)
 
     def getErrorLogList(self, logList):
         resutl = []
@@ -50,9 +77,5 @@ class Log_Reader:
         return resutl
 
 
-logreader = Log_Reader(path)
-# logreader.readLogName()
-# logreader.readErrorCode()
-logreader.readLog()
-# print(logreader.list)
-print(logreader.loglist[0:2])
+logreader = LogReader(path)
+print(logreader.getThreadFailed())
